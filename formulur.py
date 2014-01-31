686290b4118553b5c1e1ff49b3185af0dca9ac9e
steinunn8 <stf7@hi.is>
2014-01-31 15:52:08 +0000

    Formúlur fyrir lán og sparnað
    Formúluskjal svo við séum ekki allar að vinna í því sama

---
+++ formulur.py
@@ -0,0 +1,26 @@
+#Formúlur fyrir 1 mánuð
+
+		#Formúla fyrir vertryggt lán
+
+
+
+
+
+		#Formúla fyrir óverðtryggt lán
+		#Dæmi
+		int nidurstada = hofudstoll * vextir + ......;
+
+
+
+
+		#Formúla fyrir verðtryggðan 
+
+
+
+#Formúlur fyrir 1 ár
+
+		#Formúla fyrir verðtryggt lán
+
+
+
+		#.............
