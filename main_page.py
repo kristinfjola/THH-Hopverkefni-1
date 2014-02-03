8137a5455a978f4c38b5362562b7db693ad95d76
Kristin Fjola <kristinfjolato@gmail.com>
2014-02-03 11:31:39 +0000

    Útlitsbreyting, eitt fall komið

--- main_page.py
+++ main_page.py
@@ -1,4 +1,3 @@
-# -*- coding: cp1252 -*-
 import wx
 
 class MainPage(wx.Panel):
@@ -10,11 +9,12 @@ class MainPage(wx.Panel):
         grid = wx.GridBagSizer(hgap=5, vgap=5)
         hSizer = wx.BoxSizer(wx.HORIZONTAL)
 
+
         # fyrirsögn
         self.fyrirsogn = wx.StaticText(self, label="Hvað skal gera við péningana?")
         self.fyrirsognFont = wx.Font(16, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
         self.fyrirsogn.SetFont(self.fyrirsognFont)
-        mainSizer.Add(self.fyrirsogn)
+        mainSizer.Add(self.fyrirsogn, flag=wx.ALL|wx.EXPAND, border=10)
 
         mainSizer.Add(grid, 0, wx.ALL, 5)
 
@@ -30,6 +30,7 @@ class MainPage(wx.Panel):
 
         self.verdtryggtLan = wx.StaticText(self, label="Verðtryggt/óverðtryggt")
         grid.Add(self.verdtryggtLan, pos=(0,6))
+        
         '''
         "HARÐKÓÐAÐ"
         # inputs og valmöguleikar fyrir lán
@@ -49,56 +50,61 @@ class MainPage(wx.Panel):
         grid.Add(self.kr12, pos=(1,5))
         
         radioList1 = ['Verðtryggt', 'Óverðtryggt']
-        #self.radios1 = wx.RadioBox(self, choices=radioList1,  majorDimension=3, style=wx.RA_SPECIFY_COLS)
         self.radios1 = wx.RadioBox(self, choices=radioList1)
-        #grid.Add(self.radios1, pos=(1,3), span=(1,2))
         grid.Add(self.radios1, pos=(1,6))
 
         '''
         self.radioList = ['Verðtryggt', 'Óverðtryggt']
         
-        # getum haft þetta grid "interactive" með þessu
-        self.fjoldiLana = 3
-        
-        for i in range(1, self.fjoldiLana+1):
+        for i in range(1, 4):
             # innsláttur fyrir lán - self.lani
             lan = 'lan' + str(i)
             setattr(self, lan, wx.TextCtrl(self, size = (80,20)))  
-            grid.Add(object.__getattribute__(self, lan), pos=(i,0))
+            grid.Add(object.__getattribute__(self, lan), pos=(i,0), flag=wx.TOP, border=17)
+            self.Bind(wx.EVT_TEXT, self.lan_upph, object.__getattribute__(self, lan))
             
             # kr. merki fyrir lánsupphæð - self.kronai1
             krona1 = 'krona' + str(i) + '1'
             setattr(self, krona1, wx.StaticText(self, label='kr.'))
-            grid.Add(object.__getattribute__(self, krona1), pos=(i,1))
+            grid.Add(object.__getattribute__(self, krona1), pos=(i,1), flag=wx.TOP, border=17)
             
             # innsláttur fyrir vexti - self.vextiri
             vextir = 'vextir' + str(i)
             setattr(self, vextir, wx.TextCtrl(self, size = (50,20)))
-            grid.Add(object.__getattribute__(self, vextir), pos=(i,2))
+            grid.Add(object.__getattribute__(self, vextir), pos=(i,2), flag=wx.TOP, border=17)
 
             # prósentumerki - self.prosentai
             prosenta = 'prosenta' + str(i)
             setattr(self, prosenta, wx.StaticText(self, label='%'))
-            grid.Add(object.__getattribute__(self, prosenta), pos=(i,3))
+            grid.Add(object.__getattribute__(self, prosenta), pos=(i,3), flag=wx.TOP, border=17)
 
             # greiðslubyrgði - self.greidslubyrgdii
             greidslubyrgdi = 'greidslubyrgdi' + str()
             setattr(self, greidslubyrgdi, wx.TextCtrl(self, size = (80,20)))
-            grid.Add(object.__getattribute__(self, greidslubyrgdi), pos=(i,4))
+            grid.Add(object.__getattribute__(self, greidslubyrgdi), pos=(i,4), flag=wx.TOP, border=17)
             
             # kr. merki fyrir greiðslubyrgði - self.kronai2
             krona2 = 'krona' + str(i) + '2'
             setattr(self, krona2, wx.StaticText(self, label='kr.'))
-            grid.Add(object.__getattribute__(self, krona2), pos=(i,5))
+            grid.Add(object.__getattribute__(self, krona2), pos=(i,5), flag=wx.TOP, border=17)
 
             # radio buttons fyrir verðtryggingu - self.verdtryggingi
             verdtrygging = 'verdtrygging' + str(i)
             setattr(self, verdtrygging, wx.RadioBox(self, choices=self.radioList))
-            grid.Add(object.__getattribute__(self, verdtrygging), pos=(i,6))
+            grid.Add(object.__getattribute__(self, verdtrygging), pos=(i,6), flag=wx.FIXED_MINSIZE | wx.ALIGN_TOP)
             
         
-        
+
         self.SetSizerAndFit(mainSizer)
+        
+    def lan_upph(self, event):
+        widget = event.GetEventObject()
+        if (widget == self.lan1):
+            lan1_upph = event.GetString()
+        if (widget == self.lan2):
+            lan2_upph = event.GetString()
+        if (widget == self.lan3):
+            lan3_upph = event.GetString()
         
 
 app = wx.App(False)
