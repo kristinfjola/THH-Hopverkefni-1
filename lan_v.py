3d012b55d6f865cf5517342a766711ba637b5c91
Jóhanna Agnes <johannaagma@gmail.com>
2014-02-12 20:47:30 +0000

    lan_v uppfæringar og unittest(á eftir að prufa það)

--- lan_v.py
+++ lan_v.py
@@ -1,10 +1,7 @@
 # -*- coding: cp1252 -*-
 # -*- coding: utf-8 -*-
 
-# hafa if eitthvað ef það er ýtt 2x á reikna
-
-
-#global breyta sem heldur utan lántökukostnaðinn (verðbólga, vextir, uppgreiðslugjald..)
+#global breyta sem heldur utan um nöfn lána og lántökukostnaðinn (verðbólga, vextir, uppgreiðslugjald)
 kostnadur = []
 #global breyta sem heldur utan um nöfn lána og raunvexti þeirra
 lan_uppl = []
@@ -26,8 +23,9 @@ def raunvLan():
 
 	
 def lan(H, v, gb, n, verdtrygging, jafnar, verdbolga, umfram, einman, nafnLan):
-	global lan
+	global lan_uppl
 	
+	#setja inn rétta verðbólgu
 	if(verdbolga == 0): 		#verðbólga núna
 		vb = 3.1
 	elif(verdbolga == 5): 		#verðbólga 5 ár
@@ -39,11 +37,13 @@ def lan(H, v, gb, n, verdtrygging, jafnar, verdbolga, umfram, einman, nafnLan):
 	else:
 		vb = 0.0
 	
+	#setja inn nafn lána og raunvexti
 	if(verdtrygging == 0):
 		lan_uppl.append([nafnLan, v/100.0])
 	else:
 		lan_uppl.append([nafnLan, (v+vb)/100.0])
 	
+	#athuga hvaða fall á að kalla á
 	if(einman == 0):	#umframgreiðslan er eingreiðsla
 		if(jafnar == 1):
 			if(verdtrygging == 0):
@@ -76,25 +76,28 @@ def overdAfborganirEin(H, n, v, umfram, nafn):
 		v = float(v)/100
 		nt = n*12
 		vt = float(v)/12
+		
 		if(nt == 0):
 			afb = 0
 		else:
 			afb = float(H)/nt
+		
 		eftirs = H
 		summa = 0
 		stodur = [eftirs]
-		x = []
-		skil = []
+		
 		#Fyrsti mánuðurinn:
 		greidsla = afb + vt*eftirs + umfram
 		eftirs = eftirs - afb - 0.99*umfram
 		summa = summa + greidsla
 		stodur.append(round(eftirs))
+		
 		while(round(eftirs) > afb):
 			greidsla = afb + vt*eftirs
 			eftirs = eftirs - afb
 			summa = summa + greidsla
 			stodur.append(round(eftirs))
+		
 		#Síðasti mánuðurinn:
 		afb = eftirs
 		greidsla = afb + vt*eftirs
@@ -105,14 +108,15 @@ def overdAfborganirEin(H, n, v, umfram, nafn):
 		auka = [nafn, summa - H]
 		kostnadur.append(auka)
 		
+		x = []
+		skil = []
 		for i in range(0, len(stodur)):
 			x.append(i)
 		skil.append(x)
 		skil.append(stodur)
-		print skil
 		return skil
+		
 	if(n == 0):
-		print [[],[]]
 		return [[],[]]
 	else:
 		if(H > 0.99*umfram+H/(n*12)):
@@ -131,25 +135,28 @@ def verdAfborganirEin(H, n, v, vb, umfram, nafn):
 		nt = n*12
 		vt = float(v)/12
 		vbt = float(vb)/12
+		
 		if(n == 0):
 			afb = 0
 		else:
 			afb = float(H)/nt
+		
 		eftirs = H
 		summa = 0
 		stodur = [eftirs]
-		x = []
-		skil = []
+		
 		#Fyrsti mánuðurinn:
 		greidsla = afb + (vt+vbt)*eftirs + umfram
 		eftirs = eftirs - afb - 0.99*umfram
 		summa = summa + greidsla
 		stodur.append(round(eftirs))
+		
 		while(round(eftirs) > afb):
 			greidsla = afb + (vt+vbt)*eftirs
 			eftirs = eftirs - afb
 			summa = summa + greidsla
 			stodur.append(round(eftirs))
+		
 		#Síðasti mánuðurinn:
 		afb = eftirs
 		greidsla = afb + (vt+vbt)*eftirs
@@ -160,14 +167,15 @@ def verdAfborganirEin(H, n, v, vb, umfram, nafn):
 		auka = [nafn, summa - H]
 		kostnadur.append(auka)
 		
+		x = []
+		skil = []
 		for i in range(0, len(stodur)):
 			x.append(i)
 		skil.append(x)
 		skil.append(stodur)
-		print skil
 		return skil
+		
 	if(n == 0):
-		print [[],[]]
 		return [[],[]]
 	else:
 		if(H > 0.99*umfram+H/(n*12)):
@@ -183,25 +191,27 @@ def overdGreidslurEin(H, n, v, umfram, nafn):
 	v = float(v)/100
 	nt = n*12
 	vt = float(v)/12
+	
 	def temp(H, n, v, umfram):
 		greidsla = A
 		eftirs = H
 		summa = 0
 		stodur = [eftirs]
-		x = []
-		skil = []
+
 		#Fyrsti mánuðurinn:
 		vextir = v*eftirs
 		afb = greidsla - vextir + 0.99*umfram
 		eftirs = eftirs - afb
 		summa = summa + greidsla + umfram
 		stodur.append(round(eftirs))
+		
 		while(round(eftirs) > greidsla - vextir):
 			vextir = v*eftirs
 			afb = greidsla - vextir
 			eftirs = eftirs - afb
 			summa = summa + greidsla
 			stodur.append(round(eftirs))
+		
 		#Síðasti mánuðurinn:
 		afb = eftirs
 		greidsla = afb + v*eftirs
@@ -212,14 +222,15 @@ def overdGreidslurEin(H, n, v, umfram, nafn):
 		auka = [nafn, summa - H]
 		kostnadur.append(auka)
 		
+		x = []
+		skil = []
 		for i in range(0, len(stodur)):
 			x.append(i)
 		skil.append(x)
 		skil.append(stodur)
-		print skil
 		return skil
+		
 	if(n == 0):
-		print [[],[]]
 		return [[],[]]
 	elif(vt == 0):
 		overdAfborganirEin(H, nt, 0, umfram, nafn)
@@ -241,13 +252,13 @@ def verdGreidslurEin(H, n, v, vb, umfram, nafn):
 	nt = n*12
 	vt = float(v)/12
 	vbt = float(vb)/12
+	
 	def temp(H, n, v, vb, umfram):
 		eftirs = H
 		greidsla = A
 		summa = 0
 		stodur = [eftirs]
-		x = []
-		skil = []
+	
 		#Fyrsti mánuðurinn:
 		eftirs = eftirs + vb*eftirs
 		greidsla = (1+vb)*greidsla
@@ -255,6 +266,7 @@ def verdGreidslurEin(H, n, v, vb, umfram, nafn):
 		eftirs = eftirs - afb
 		summa = summa + greidsla + umfram
 		stodur.append(round(eftirs))
+		
 		while(round(eftirs) > greidsla - v*eftirs):
 			eftirs = eftirs + vb*eftirs
 			greidsla = (1+vb)*greidsla
@@ -262,6 +274,7 @@ def verdGreidslurEin(H, n, v, vb, umfram, nafn):
 			eftirs = eftirs - afb
 			summa = summa + greidsla
 			stodur.append(round(eftirs))
+		
 		#Síðasti mánuðurinn:
 		afb = eftirs
 		greidsla = afb + v*eftirs
@@ -272,14 +285,15 @@ def verdGreidslurEin(H, n, v, vb, umfram, nafn):
 		auka = [nafn, summa - H]
 		kostnadur.append(auka)
 		
+		x = []
+		skil = []
 		for i in range(0, len(stodur)):
 			x.append(i)
 		skil.append(x)
 		skil.append(stodur)
-		print skil
 		return skil
+		
 	if(n == 0):
-		print [[],[]]
 		return [[],[]]
 	elif(vbt == 0):
 		overdGreidslurEin(H, nt, vt, umfram, nafn)
@@ -300,22 +314,23 @@ def overdAfborganirMan(H, n, v, umfram, nafn):
 	v = float(v)/100
 	nt = n*12
 	vt = float(v)/12
+	
 	if(nt == 0):
-		print [[],[]]
 		return [[],[]]
+	
 	else:
 		afb = float(H)/nt
 		eftirs = H
 		summa = 0
 		stodur = [eftirs]
-		x = []
-		skil = []
+		
 		#keyrum þetta á meðan summa afborgunar og umframgreiðslu er minni en eftirstaðan (svo eftirstaðan fer ekki í mínustölu)
 		while(round(eftirs) > afb+0.99*umfram):
 			greidsla = afb + vt*eftirs + umfram
 			eftirs = eftirs - afb - 0.99*umfram		#0.99 útaf 1% fer í uppgreiðslukosntaður
 			summa = summa + greidsla
 			stodur.append(round(eftirs))
+		
 		#síðasti mánuðurinn: ef afborgnunin er meiri en eftirstaðan verður engin umframgreiðsla
 		if(afb > eftirs):
 			afb = eftirs
@@ -323,6 +338,7 @@ def overdAfborganirMan(H, n, v, umfram, nafn):
 			eftirs = eftirs - afb
 			summa = summa + greidsla
 			stodur.append(round(eftirs))
+		
 		#síðasti mánuðurinn: ef afborgunin er minni, þá þurfum við fyrst að reikna hver umframgreiðslan þarf að vera
 		else:
 			umfram = (eftirs - afb)/0.99
@@ -334,11 +350,12 @@ def overdAfborganirMan(H, n, v, umfram, nafn):
 		auka = [nafn, summa - H]
 		kostnadur.append(auka)
 		
+		x = []
+		skil = []
 		for i in range(0, len(stodur)):
 			x.append(i)
 		skil.append(x)
 		skil.append(stodur)
-		print skil
 		return skil
 
 
@@ -352,22 +369,23 @@ def verdAfborganirMan(H, n, v, vb, umfram, nafn):
 	nt = n*12
 	vt = float(v)/12
 	vbt = float(vb)/12
+	
 	if(n == 0):
-		print []
-		return []
+		return [[],[]]
+	
 	else:
 		afb = float(H)/nt
 		eftirs = H
 		summa = 0
 		stodur = [eftirs]
-		x = []
-		skil = []
+		
 		#keyrum þetta á meðan summa afborgunar og umframgreiðslu er minni en eftirstaðan (svo eftirstaðan fer ekki í mínustölu)
 		while(round(eftirs) > afb+0.99*umfram):
 			greidsla = afb + (vt+vbt)*eftirs + umfram
 			eftirs = eftirs - afb - 0.99*umfram		#0.99 útaf 1% fer í uppgreiðslukosntaður
 			summa = summa + greidsla
 			stodur.append(round(eftirs))
+		
 		#síðasti mánuðurinn: ef afborgnunin er meiri en eftirstaðan verður engin umframgreiðsla
 		if(afb > eftirs):
 			afb = eftirs
@@ -375,6 +393,7 @@ def verdAfborganirMan(H, n, v, vb, umfram, nafn):
 			eftirs = eftirs - afb
 			summa = summa + greidsla
 			stodur.append(round(eftirs))
+		
 		#síðasti mánuðurinn: ef afborgunin er minni, þá þurfum við fyrst að reikna hver umframgreiðslan þarf að vera
 		else:
 			umfram = (eftirs - afb)/0.99
@@ -386,11 +405,12 @@ def verdAfborganirMan(H, n, v, vb, umfram, nafn):
 		auka = [nafn, summa - H]
 		kostnadur.append(auka)
 		
+		x = []
+		skil = []
 		for i in range(0, len(stodur)):
 			x.append(i)
 		skil.append(x)
 		skil.append(stodur)
-		print skil
 		return skil
 
 
@@ -401,13 +421,13 @@ def overdGreidslurMan(H, n, v, umfram, nafn):
 	v = float(v)/100
 	nt = n*12
 	vt = float(v)/12
+	
 	def temp(H, n, v, umfram):
 		greidsla = A
 		eftirs = H
 		summa = 0
 		stodur = [eftirs]
-		x = [[],[]]
-		skil = [[],[]]
+
 		#keyrum þetta á meðan afborgunin = greidsla-v*eftirs+0.99*umfram er minni en eftirstaðan
 		while(round(eftirs) > greidsla-v*eftirs+0.99*umfram):
 			afb = greidsla - v*eftirs + 0.99*umfram		#þar sem v*eftirs = vextir #0.99 útaf 1% fer í uppgreiðslukosntaður
@@ -432,15 +452,16 @@ def overdGreidslurMan(H, n, v, umfram, nafn):
 		auka = [nafn, summa - H]
 		kostnadur.append(auka)
 		
+		x = []
+		skil = []
 		for i in range(0, len(stodur)):
 			x.append(i)
 		skil.append(x)
 		skil.append(stodur)
-		print skil
 		return skil
+		
 	#áður en við förum í temp þarf að ath hvort n=0 eða v=0 svo við séum ekki að deila með 0 og fá keyrsluvillu
 	if(n == 0):
-		print [[],[]]
 		return [[],[]]
 	elif(v == 0):
 			overdAfborganirMan(H, n, 0, umfram, nafn)
@@ -462,13 +483,13 @@ def verdGreidslurMan(H, n, v, vb, umfram, nafn):
 	nt = n*12
 	vt = float(v)/12
 	vbt = float(vb)/12
+	
 	def temp(H, n, v, vb, umfram):
 		eftirs = H
 		greidsla = A
 		summa = 0
 		stodur = [eftirs]
-		x = [[],[]]
-		skil = [[],[]]
+		
 		#keyrum þetta á meðan afborgunin = greidsla-v*eftirs+0.99*umfram er minni en eftirstaðan(með viðbættri verðbólgu)
 		while(round((1+vb)*eftirs) > (1+vb)*greidsla-v*eftirs+0.99*umfram): 	
 			eftirs = (1+vb)*eftirs
@@ -477,6 +498,7 @@ def verdGreidslurMan(H, n, v, vb, umfram, nafn):
 			eftirs = eftirs - afb
 			summa = summa + greidsla + umfram
 			stodur.append(round(eftirs))
+		
 		#síðasti mánuðurinn: ef afborgnunin = greidsla-v*eftirs er meiri en eftirstaðan (með verðbólgu) verður engin umframgreiðsla
 		if(greidsla-v*eftirs > (1+vb)*eftirs):
 			eftirs = (1+vb)*eftirs
@@ -498,15 +520,16 @@ def verdGreidslurMan(H, n, v, vb, umfram, nafn):
 		auka = [nafn, summa - H]
 		kostnadur.append(auka)
 		
+		x = []
+		skil = []
 		for i in range(0, len(stodur)):
 			x.append(i)
 		skil.append(x)
 		skil.append(stodur)
-		print skil
 		return skil
+		
 	#áður en við förum í temp þarf að ath hvort n=0 eða v=0 svo við séum ekki að deila með 0 og fá keyrsluvillu
 	if(n == 0):
-		print [[],[]]
 		return [[],[]]
 	elif(vb == 0):
 			overdGreidslurMan(H, n, v, umfram, nafn)
---
+++ unittest.py
@@ -0,0 +1,24 @@
+import unittest
+import sparnadur1b
+import lan_v
+
+class testMaxFunctions(unittest.TestCase):
+	def test_raunvx(self):
+		self.assertEqual(sparnadur1b.raunvx(0, 12, 0.01), 0.036)
+		self.assertEqual(sparnadur1b.raunvx(0, 18, 0.02), 0.028)
+		self.assertEqual(sparnadur1b.raunvx(1, 36, 0.05), 0.0175)
+		self.assertEqual(sparnadur1b.raunvx(1, 36, 0.09), 0.0175)
+	def test_sparivx(self):
+		self.assertEqual(sparnadur1b.sparivx(0, 12), 0.046)
+		self.assertEqual(sparnadur1b.sparivx(0, 24), 0.053)
+		self.assertEqual(sparnadur1b.sparivx(1, 36), 0.0175)
+		self.assertEqual(sparnadur1b.sparivx(1, 48), 0.0185)
+	def test_hvad_er_best_ad_gera(self):
+		self.assertEqual(sparnadur1b.hvad_er_best_ad_gera(12, 0.05), 'Ad borga inn a Overdtryggdur 12')
+		self.assertEqual(sparnadur1b.hvad_er_best_ad_gera(36, 0.08), 'Ad borga inn a Verdtryggdur 36')	
+	def test_overdAfborganirMan(self):
+		self.assertEqual(lan_v.overdAfborganirMan(10000, 0, 5, 500, 'Lan 1'), [[], []])
+		self.assertEqual(lan_v.overdAfborganirMan(500000, 0, 3, 25000, 'Lan 2'), [[], []])	
+		
+	if __name__ == '__main__':
+		unittest.main(verbosity=2, exit=False)
