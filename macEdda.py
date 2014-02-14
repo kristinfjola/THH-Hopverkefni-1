ed24675217180e96b2afd5f974825b58eadf3ff7
eddabjork <ebk13@hi.is>
2014-02-14 16:12:49 +0000

    Update macEdda.py

--- macEdda.py
+++ macEdda.py
@@ -18,10 +18,10 @@ class MainPage(wx.Panel):
     def reikna(self, event):
       #bara þessi lína breyttist:
     	self.nidurst = sparnadur1b.spar(self.umframgr, self.verdbolga, self.verdSparn, self.ein_man_greidsla, self.innist_bundin)
+    	self.lanNidurst = lan_v.lan(self.lan1_upph, self.lan1_vextir, self.lan1_greidslubyrgdi, self.lan1_timabil, self.lan1_verdtrygging, self.lan1_jafnar, self.verdbolga, self.umframgr, self.ein_man_greidsla, "Lan 1")
     	
     	#!!!!!!
-    	#ekki copy-a eftirfarandi 4 línur :)
-    	lan_v.lan(self.lan1_upph, self.lan1_vextir, self.lan1_greidslubyrgdi, self.lan1_timabil, self.lan1_verdtrygging, self.lan1_jafnar, self.verdbolga, self.umframgr, self.ein_man_greidsla)
+    	#ekki copy-a eftirfarandi 3 línur :)
     	lan_v.lan(self.lan2_upph, self.lan2_vextir, self.lan2_greidslubyrgdi, self.lan2_timabil, self.lan2_verdtrygging, self.lan2_jafnar, self.verdbolga, self.umframgr, self.ein_man_greidsla)
     	lan_v.lan(self.lan3_upph, self.lan3_vextir, self.lan3_greidslubyrgdi, self.lan3_timabil, self.lan3_verdtrygging, self.lan3_jafnar, self.verdbolga, self.umframgr, self.ein_man_greidsla)
     	self.syna_svar_glugga()
@@ -30,16 +30,22 @@ class MainPage(wx.Panel):
    
     def fa_nidurstodur(self):
     	return self.nidurst
+    
+    def fa_nidurstodur_ur_lani(self):
+    	return self.lanNidurst
 
 class SvarGluggi(wx.Frame):
 
     def __init__(self,parent,id):
-        wx.Frame.__init__(self, parent, id, 'Svör', size=(800,650))
+        wx.Frame.__init__(self, parent, id, 'Svor', size=(800,650))
         wx.Frame.CenterOnScreen(self)
         
+        self.scroll = wx.ScrolledWindow(self, -1) #NÝTT
+        self.scroll.SetScrollbars(1, 1, 800, 650) #NÝTT
+        
         fig, ax = plt.subplots()
-
-        panel_svar = wx.Panel(self)
+        
+        panel_svar = wx.Panel(self.scroll) #NÝTT
         sizer = wx.BoxSizer(wx.VERTICAL)
         canvas = FigCanvas(panel_svar, -1, fig)
         
@@ -55,22 +61,60 @@ class SvarGluggi(wx.Frame):
         
         ax.plot(data_x, data_y, label="Reikningur ef lagt er fyrir")
         
-        ax.set_xlabel('Tími [mánuðir]')
-        ax.set_ylabel('Upphæð [kr]')
-        ax.set_title('Reyndu nú að spara!')
+        ax.set_xlabel('Timi (manudir)')
+        ax.set_ylabel('Upphaed')
+        ax.set_title('Reyndu nu ad spara')
         ax.legend(loc=2); # upper left corner
         
         canvas.draw()
-        sizer.Add(canvas, 1, wx.GROW)
+        
+        nidurstodur2 = MainPage.fa_nidurstodur_ur_lani(panel)
+        
+        if(size(nidurstodur2) != 0):
+        	fig2, ax2 = plt.subplots()
+        	canvas2 = FigCanvas(panel_svar, -1, fig2)
+        	xmax = size(nidurstodur2[0])-1
+        	ymin = size(nidurstodur2[1])-1
+        	ax2.set_ylim([nidurstodur2[1][ymin], nidurstodur2[1][0]])
+        	ax2.set_xlim([0, nidurstodur2[0][xmax]])
+        
+        	data_x2 = nidurstodur2[0]
+        	data_y2 = nidurstodur2[1]
+        
+        	ax2.plot(data_x2, data_y2, label="Lan ef borgad er inna thad")
+        
+       	 	ax2.set_xlabel('Timi (manudir)')
+        	ax2.set_ylabel('Upphaed')
+        	ax2.set_title('Reyndu nu ad spara')
+        	ax2.legend(loc=1); # upper right corner
+        
+        	canvas2.draw()
 
         #...
-
         
-        panel_svar.SetSizerAndFit(sizer)
+        lanakostnadur = wx.StaticText(panel_svar, -1, "Auka kostnadur vid lan (vextir, uppgreidslugjald): " )#+ lana_kostnadur)
+        sizer.Add(lanakostnadur, 0, wx.ALL, 10)
+        
+        sizer.Add(canvas, 0, wx.ALL, 10) #NÝTT -> nýr staður
+        if(size(nidurstodur2) != 0):
+        	sizer.Add(canvas2, 0, wx.ALL, 10)
+        
+        # !! NÝTT !!! (út þetta fall)
+        
+        panel_svar.SetSizer(sizer)
+        panel_svar.SetAutoLayout(True)
         panel_svar.Layout()
+        panel_svar.Fit()
+        
+        self.Center()
+        self.MakeModal( True )
+        
+        self.frmPanelWid, self.frmPanelHgt = panel_svar.GetSize()
+        self.unit = 1
+        self.scroll.SetScrollbars( self.unit, self.unit, self.frmPanelWid/self.unit, self.frmPanelHgt/self.unit )
         
 app = wx.App(False)
-frame = wx.Frame(None, title="Önnur útgáfa")
+frame = wx.Frame(None, title="Onnur utgafa")
 frame.SetSize((850,500))
 panel = MainPage(frame)
 frame.Show()
