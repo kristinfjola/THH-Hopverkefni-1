4629738193b840c1318106f29b05cbce4350add1
Steinunn <stf7@hi.is>
2014-02-19 10:46:21 +0000

    Úr mánuðum í ár
    a

--- sparitimi.py
+++ sparitimi.py
@@ -1,14 +1,14 @@
 # -*- coding: cp1252 -*-
 # -*- coding: utf-8 -*-
+## Í þessu skjali er virknin fyrir tab 2. 
+
 import math
 globfjarmagns = 0
 globvextir = 0
 vt = 0
 
-## Kall á fall til að reikna innlogn
-## sparitimi.innlogn(upphæð sem safna á uppí, vextir, mánuðir, eingreiðsla eða ekki (0 eða 1))
-
-
+## Fall sem kallað er á úr viðmóti til þess að fá upp hversu mikið þarf að leggja inn til þess að ná uppí tiltekna upphæð á tilteknum tíma.
+## sparitimi.innlogn(upphæð sem safna á uppí, vextir, ár, eingreiðsla eða ekki (0 eða 1))
 def innlogn(heild, v, nt, manada):
 	v = v/100.0
 	nt = nt * 12
@@ -17,58 +17,70 @@ def innlogn(heild, v, nt, manada):
 	else:
 		return eingreidslaSparInnlogn(nt, v, heild)
 
-## Kall á fall til að reikna timabil
-## sparitimi.timabil(Upphæð sem leggja á inn á mánuði, vextir, upphæð sem safna á uppí, eingreiðsla eða ekki (0 eða 1))
 
+## Fall sem kallað er á úr viðmóti til þess að fá upp hversu lengi þarf að spara til þess að ná uppí tiltekna upphæð þegar tiltekin upphæð er lögð inn.
+## sparitimi.timabil(Upphæð sem leggja á inn á mánuði, vextir, upphæð sem safna á uppí, eingreiðsla eða ekki (0 eða 1))
 def timabil(L, v, heild, manada):
 	global vt
 	v = v/100.0
-	nt = nt * 12
 	if manada:
 		return manadalegurSparTimi(L, v, vt, heild)
 	else:
 		return eingreidslaSparTimi(L, v, vt, heild)
 
 
-
-
-
-
 ## Fall sem skilar hversu mikið þú þarft að leggja inn á mánuði til þess að eiga ákveðna upphæð eftir eitthvað marga mánuði
-"""
-Notkun: 
-Fyrir:
-Eftir:
-"""
+
+##Notkun: innlogn = manadalegurSparInnlogn(nt, v, heild)
+##Fyrir: nt er timabil i arum, v er vextir a float formi (0.0X fyrir X prosent vexti) og heild er upphaed sem safna a uppi
+##Eftir: innlogn er strengur sem segir notanda hversu mikid hann tharf ad leggja inn a manudi
 def manadalegurSparInnlogn(nt, v, heild):
 	temp = 0.0
 	i = 0
 	for i in range(1, nt+1):
-		temp = temp + (1+(v/12)*0.9)**i
-	return "Thu tharft ad spara " + str(math.ceil(1.0*heild/temp)) + " a manudi til thess ad komast uppi " + str(heild)
+		temp = temp + (1+(v/12))**i
+	return "Thu tharft ad spara " + str(math.ceil(1.0*heild/temp)) + " kr. a manudi til thess ad komast uppi " + str(heild) + " kr."
 
 
-## Fall sem skilar hversu mikið þú þarft að leggja inn í upphafi tiþess að eiga ákveðna upphæð eftir eitthvað marga mánuði
-"""
-Notkun:
-Fyrir:
-Eftir:
-"""
+## Fall sem skilar hversu mikið þú þarft að leggja inn í upphafi til þess að eiga ákveðna upphæð eftir eitthvað marga mánuði
+
+##Notkun: innlogn = eingreidslaSparInnlogn(nt, v, heild)
+##Fyrir: nt er timabil i arum, v er vextir a float formi (0.0X fyrir X prosent vexti) og heild er upphaed sem safna a uppi
+##Eftir: innlogn er strengur sem segir notanda hversu mikið hann tharf ad leggja inn i upphafi
 def eingreidslaSparInnlogn(nt, v, heild):
 	innlogn = heild/(pow((1+v),(nt/12.0)))
-	return "Thu tharft ad leggja inn "+str(math.ceil(innlogn)) + " i upphafi til thess ad na uppi " + str(heild)
+	return "Thu tharft ad leggja inn "+ str(math.ceil(innlogn)) + " kr. i upphafi til thess ad na uppi " + str(heild) + " kr."
 
 
-##heild = innlogn * pow((1+v), nt/12))
+##Fall sem skiar mánuðum í árum og mánuðum
+
+## Notkun: strengur = manudirIAr(tala)
+## Fyrir: tala >= 0
+## Eftir: strengur er tala skippt niður í mánuði og ár
+def manudirIAr(tala):
+	ar = tala / 12.0
+	man = tala % 12.0
+	if ar == 0:
+		if man == 1:
+			return str(man) + " manud"
+		else:	
+			return str(man) + " manudi"
+	elif man == 0:
+		return str(ar) + " ar"
+	else:
+		if man == 1:
+			return str(ar) + " ar og " + str(man) + " manud"
+		else:	
+			return str(ar) + " ar og " + str(man) + " manudi"
+
 
 ## Timinn sem tekur að spara uppí ákveðna upphæð
-## Fallið gerir ráð fyrir vöxtum, verðbólgu og fjármagnstekjuskatti
-## (og er því ekki í samræmi við bankareiknivélar því flestar gera ekki ráð fyrir verðbólgu eða fjármagnstekjuskatti)
-"""
-Notkun: timi = manadalegurSpar(L, v, vt, heild)
-Fyrir: L er mánaðaleg innlögn, v er ársvextir, vt er verðbólga og heild er upphæð sem notandi vill safna uppí.
-Eftir: timi er sá tími sem tekur að safna uppí heild með L innlögn á mánuði. timi er strengur í mánuðum.
-"""
+## Fallið gerir ráð fyrir vöxtum greiddum árlega, verðbólgu og þar með verðbótum greiddum mánaðalega ef reikningur er verðtryggður og fjármagnstekjuskatti sem er dreginn frá árlega.
+## Ekki í samræmi við bankanetreiknivélar þar sem þar eru vextir greiddir út strax. (Þessi aðferð segir lengri tíma)
+
+## Notkun: timi = manadalegurSpar(L, v, vt, heild)
+## Fyrir: L er mánaðaleg innlögn, v er ársvextir, vt er verðbólga og heild er upphæð sem notandi vill safna uppí.
+## Eftir: timi er strengur sem segir notanda hversu lengi hann er að safna uppí tiltekna upphæð með tiltekinni innlögn á mánuði
 def manadalegurSparTimi(L, v, vt, heild):
 	global globvextir
 	global globfjarmagns
@@ -81,9 +93,11 @@ def manadalegurSparTimi(L, v, vt, heild):
 	verdbaeturAr = []
 	for i in range(0,10000):
 		if summa >= heild:
-			return "Thu tarft ad spara i "+str(i)+ " manudi til thess ad na uppi "+ str(heild)
+			manAr = manudirIAr(i)
+			return "Thu tarft ad spara i "+ str(manAr) + " til thess ad na uppi "+ str(heild) + " kr."
 		elif i == 9999:
-			return "Thu tharft ad spara i "+ str(i) + " manudi eda lengur"
+			manAr = manudirIAr(i)
+			return "Thu tharft ad spara i "+ str(manAr) + " eda lengur. Reyndu ad leggja meira fyrir."
 		summa = (summa + L)*(1+(vt/12))
 		verdbaetur.append(summa*(vt/12))
 		verdbaeturAr.append(summa*(vt/12))
@@ -100,13 +114,11 @@ def manadalegurSparTimi(L, v, vt, heild):
 	globfjarmagns = sum(vextir)*(1-fjarmagns) + sum(verdbaetur)*(1-fjarmagns)
 	return "Virkaði ekki"
 
+## Fall sem skilar tíma sem það tekur að safna uppí ákveðna upphæð með eingreiðslu
 
-## Fall sem skilar tíma sem það tekur að safna uppí ákveðna upphæð með .....................
-"""
-Notkun: tala = eingreidsla(L, nt, v, vt)
-Fyrir:  
-Eftir: 
-"""
+## Notkun: timi = eingreidsla(L, nt, v, vt)
+## Fyrir: L er mánaðaleg innlögn, v er ársvextir, vt er verðbólga og heild er upphæð sem notandi vill safna uppí.
+## Eftir: timi er strengur sem segir notanda hversu lengi hann er að safna uppí tiltekna upphæð með tiltekinni eigreiðslu.
 def eingreidslaSparTimi(L, v, vt, heild):
 	global globvextir
 	global globfjarmagns
@@ -121,9 +133,11 @@ def eingreidslaSparTimi(L, v, vt, heild):
 	skil = []
 	for i in range(0, 10000):
 		if summa >= heild:
-			return "Thu tharft ad spara i " + str(i) + " manudi til thess ad na uppi " + str(heild)
+			manAr = manudirIAr(i)
+			return "Thu tharft ad spara i " + str(manAr) + " til thess ad na uppi " + str(heild) + " kr."
 		elif i == 9999:
-			return "Thu tharft ad spara i " + str(i) + " eda lengur"
+			manAr = manudirIAr(i)
+			return "Thu tharft ad spara i " + str(manAr) + " eda lengur. Reyndu ad leggja meira fyrir."
 		summa = summa * (1+(vt/12))
 		verdbaetur.append(summa*(vt/12))
 		verdbaeturAr.append(summa*(vt/12))
